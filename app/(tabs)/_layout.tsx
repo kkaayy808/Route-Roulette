5097a44170ae26eda9709a7f0aae0b55b239b3b4
Kaitlyn Asato <kaitlyn.asato.22@cnu.edu>
2025-10-07 02:53:45 -0400

    added a tab bar at bottom of page

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+﻿import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "blue",
+            tabBarStyle: { backgroundColor: "#fff" },
+        }}
+        >
+            <Tabs.Screen name="index"
+                options={{
+                    title: "Home", tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" color={color} size={size} />
+                    ),
+                }}
+            />
+
+        </Tabs>
+    );
+}
