4e7a26355648a05ad4c3dc1429b82f766d6e0fef
Kaitlyn Asato <kaitlyn.asato.22@cnu.edu>
2025-10-14 23:15:39 -0400

    changed simulator hold color

--- app/simulator.tsx
+++ app/simulator.tsx
@@ -100,7 +100,7 @@ const styles = StyleSheet.create({
         width: 25,
         height: 25,
         borderRadius: 25 / 2,
-        backgroundColor: "red",
+        backgroundColor: "grey",
         borderColor: "black",
         borderWidth: 2,
         margin: 2,
